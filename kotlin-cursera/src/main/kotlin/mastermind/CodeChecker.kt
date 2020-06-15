735682b2e3d590300286ab15276e5057b0bd2d63
alejandra <alejandra.juarez@etermax.com>
2020-06-15 16:26:54 -0300

    initial configuration

---
+++ kotlin-cursera/src/main/kotlin/mastermind/CodeChecker.kt
@@ -0,0 +1,4 @@
+package mastermind
+
+class CodeChecker {
+}
---
+++ kotlin-cursera/src/test/kotlin/mastermind/CodeCheckerTest.kt
@@ -0,0 +1,13 @@
+package mastermind
+
+import org.assertj.core.api.Assertions
+import org.jetbrains.annotations.TestOnly
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class CodeCheckerTest{
+    @Test
+    fun test(){
+        Assertions.assertThat(true).isTrue()
+    }
+}
